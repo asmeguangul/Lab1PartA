67dc36e821b5e49f6d81dfd3af48dc1416832f66
Asmelash Abrha Guangul <615157@cs.mum.edu>
2023-03-15 02:59:10 -0500

    Initial commit

---
+++ src/main/java/customers/CustomerDAO.java
@@ -0,0 +1,26 @@
+package customers;
+
+public class CustomerDAO implements ICustomerDAO{
+	private ILogger logger = new Logger();
+
+	public void save(Customer customer) {
+		// simple sleep
+		try {
+			Thread.sleep(350);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("CustomerDAO: saving customer "+customer.getName());
+		logger.log("Customer is saved in the DB: "+ customer.getName() );
+	}
+
+}
+
+
+//	private Logger logger;
+//	//	private ILogger logger = new Logger();
+//
+//
+//	public CustomerDAO(Logger logger) {
+//		this.logger = logger;
+//	}
